25589cd12c0f0241383e11c907f7365e7378d838
GABRIEL MONTEIRO DE SOUZA <gabriel_m_de-souza@estudante.sc.senai.br>
2023-12-05 19:47:36 -0300

    alteração no footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
     return (
         <footer className='footer'>
             <p  className='contact-title'>Contato</p>
-            <p>top10movie@gmail.com</p>
-            <p>(48) 8843-6423</p>
+            <p>maria-bonita-gastronomia@gmail.com</p>
+            <p>(xx) xxxx-xxxx</p>
         </footer>
     );
 }
--- src/pages/info/Info.js
+++ src/pages/info/Info.js
@@ -4,29 +4,28 @@ import InformationText from "../../components/informationText/InformationText";
 function Info() {
     return (
         <div>
-            <h1>Informações</h1>
+            <h1>Sobre o restaurante</h1>
             <InformationText
-                h2Content="Sobre Nós"
-                pContent=" Na Top 10 Movie, Estamos entusiasmados em apresentar nosso novo
-                    serviço de anúncio dos filmes mais recentes lançados. Estamos aqui
-                    para manter você atualizado com os filmes mais populares e emocionantes
-                    que chegam às telas de cinema."
+                h2Content="Sobre nós"
+                pContent="Bem-vindo a Maria Bonita  Cozinha da Mamãe,
+                 um legado que teve início em 1970, quando Dona Maria,
+                  uma mulher apaixonada pela culinária caseira,
+                   decidiu compartilhar o calor de sua cozinha com a comunidade.
+                    Com um punhado de receitas tradicionais transmitidas de geração em geração,
+                     ela abriu as portas de seu pequeno restaurante,
+                      transformando-o em um refúgio acolhedor onde os sabores autênticos
+                       da comida caseira ganhavam vida."
             />
-            <br/>
+            <br />
             <InformationText
-                h2Content="O que oferecemos"
-                pContent="Aqui, você encontrará detalhes completos sobre os filmes,
-                    incluindo sinopses envolventes, trailers emocionantes e informações
-                    relevantes sobre elenco, direção e muito mais. Queremos garantir que
-                    você tenha todas as informações necessárias para fazer escolhas informadas
-                    ao planejar sua próxima ida ao cinema"
-            />
-            <br/>
-            <InformationText
-                h2Content="Explore nossa página web"
-                pContent="Nossa página web é o seu destino confiável para descobrir os filmes mais
-                    recentes que estão fazendo sucesso no momento. Estamos comprometidos em
-                    tornar sua experiência conosco agradável e gratificante."
+                h2Content="Nossa gastronomia"
+                pContent="Oferecemos uma experiência gastronômica autêntica e acolhedora.
+                        Nosso cardápio destaca pratos caseiros tradicionais,
+                        preparados com ingredientes frescos e sazonais. Em um ambiente caloroso,
+                        convidamos os clientes a desfrutarem de uma atmosfera familiar,
+                        onde cada refeição é uma celebração da culinária caseira e uma viagem de volta às raízes.
+                        Nossa equipe dedicada proporciona um serviço amigável,
+                        tornando cada visita uma experiência que alimenta tanto o corpo quanto a alma."
             />
         </div>
     );

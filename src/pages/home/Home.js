c749979d806e8b51a31507a9e81ceabd4f13d688
VINÍCIUS DE SOUZA DE PAIVA <vinicius_paiva@estudante.sesisenai.org.br>
2023-12-05 21:33:59 -0300

    acrecentando coisas no home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,23 @@
 import logoPrincipal from "../../img/logoPrincipal.png"
-import fundo from "../../img/fundo.jpg"
 import './Home.css'
+import InformationText from "../../components/informationText/InformationText"
 
 function Home() {
 
     return (
         <div class='inicial'>
             <h1>Restaurante Maria Bonita</h1>
-            <img src={logoPrincipal}/>
-            <img src={fundo} class="alo"/>
+
+            <div class="img">
+                <img src={logoPrincipal} />
+            </div>
+            <InformationText
+                pContent="Bem-vindos ao Maria Bonita, o restaurante que traz até você a autenticidade e o calor das comidas caseiras preparadas com carinho e tradição. Em nosso acolhedor ambiente, buscamos proporcionar uma experiência gastronômica única, onde cada prato é cuidadosamente elaborado para despertar memórias afetivas e sabores que remetem à comida feita em casa."
+            />
         </div>
-        
+
+
+
     )
 
 }

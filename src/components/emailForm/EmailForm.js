622a4d94f75762d4b554dcc210f3199e0da37886
GABRIEL MONTEIRO DE SOUZA <gabriel_m_de-souza@estudante.sc.senai.br>
2023-12-05 21:32:57 -0300

    adicionado imagens e links a pagina contato

--- src/components/emailForm/EmailForm.js
+++ src/components/emailForm/EmailForm.js
@@ -1,59 +1,39 @@
-import './emailForm.css'
-import InputAndLabel from "../inputAndLabel/InputAndLabel";
-import {useEffect, useState} from "react";
+import './emailForm.css';
+import Instagram from "../../img/Instagram.png";
+import facebook from "../../img/facebook.png";
+import whatsApp from "../../img/whatsApp.png";
 
-function EmailForm () {
-
-    const [email, setEmail] = useState('')
-    const [phone, setPhone] = useState('')
-    const [messages, setMessages] = useState('')
-    const [content, setContent] = useState([])
-
-    const handleEmail = (e) => {
-        setEmail(e.target.value)
-    }
-    const handlePhone = (e) => {
-        setPhone(e.target.value)
-    }
-    const handleContent = (e) => {
-        setMessages(e.target.value)
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const NewContent = {
-            email: email,
-            phone: phone,
-            messages: messages
-        }
-        setContent([...content, NewContent])
-        console.log(content)
-        e.target.reset()
-        console.log(content)
-        alert("Mensagem enviada!")
-
-    }
-    useEffect(() => {
-        console.log(content);
-    }, [content]);
-
-    return(
+function EmailForm() {
+    return (
         <div className="email-form-container">
-            <form onSubmit={handleSubmit}>
-                <InputAndLabel
-                    onChange={(e) => (handleEmail(e))}
-                    title="Email:"/>
-                <InputAndLabel
-                    onChange={(e) => (handlePhone(e))}
-                    title="Telefone:"/>
-                <div>
-                    <label>Mensagem:</label>
-                    <textarea
-                    onChange={(e) => (handleContent(e))}
-                    />
+            <div className="image-gallery">
+                <div className="image-container">
+                    <img src={Instagram}/>
+                    <div className="contact-buttons">
+                        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                            <button>Instagram</button>
+                        </a>
+                    </div>
                 </div>
-                <button type="submit">Enviar</button>
-            </form>
+                <div className="image-container">
+                    <img src={whatsApp}/>
+                    <div className="contact-buttons">
+                        <a href="https://api.whatsapp.com/send?phone=seu-numero" target="_blank" rel="noopener noreferrer">
+                            <button> WhatsApp </button>
+                        </a>
+                    </div>
+                </div>
+                <div className="image-container">
+                    <img src={facebook}/>
+                    <div className="contact-buttons">
+                        <a href="https://www.facebook.com/?locale=pt_BR" target="_blank" rel="noopener noreferrer">
+                            <button>Facebook</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
-export default EmailForm
+
+export default EmailForm;
--- src/pages/info/Info.js
+++ src/pages/info/Info.js
@@ -7,25 +7,26 @@ function Info() {
             <h1>Sobre o restaurante</h1>
             <InformationText
                 h2Content="Sobre nós"
-                pContent="Bem-vindo a Maria Bonita  Cozinha da Mamãe,
-                 um legado que teve início em 1970, quando Dona Maria,
-                  uma mulher apaixonada pela culinária caseira,
-                   decidiu compartilhar o calor de sua cozinha com a comunidade.
-                    Com um punhado de receitas tradicionais transmitidas de geração em geração,
-                     ela abriu as portas de seu pequeno restaurante,
-                      transformando-o em um refúgio acolhedor onde os sabores autênticos
-                       da comida caseira ganhavam vida."
+                pContent="Bem-vindos ao Maria Bonita, o restaurante que traz até você 
+                a autenticidade e o calor das comidas caseiras preparadas com carinho e
+                 tradição. Em nosso acolhedor ambiente, buscamos proporcionar uma experiência
+                  gastronômica única, onde cada prato é cuidadosamente elaborado para despertar
+                   memórias afetivas e sabores que remetem à comida feita em casa.."
             />
             <br />
             <InformationText
                 h2Content="Nossa gastronomia"
-                pContent="Oferecemos uma experiência gastronômica autêntica e acolhedora.
-                        Nosso cardápio destaca pratos caseiros tradicionais,
-                        preparados com ingredientes frescos e sazonais. Em um ambiente caloroso,
-                        convidamos os clientes a desfrutarem de uma atmosfera familiar,
-                        onde cada refeição é uma celebração da culinária caseira e uma viagem de volta às raízes.
-                        Nossa equipe dedicada proporciona um serviço amigável,
-                        tornando cada visita uma experiência que alimenta tanto o corpo quanto a alma."
+                pContent="Nossos chefs dedicam-se a selecionar os ingredientes mais frescos e de alta qualidade,
+                 garantindo que cada refeição seja uma verdadeira celebração do paladar. Do arroz e feijão ao frango assado,
+                passando pelos pratos típicos da culinária local, cada opção do cardápio reflete a paixão pela cozinha caseira
+                e o compromisso com o bem-estar dos nossos clientes.
+                No Maria Bonita, não apenas servimos comida, mas compartilhamos momentos especiais em torno da mesa.
+                Seja para um almoço em família, um jantar romântico ou uma reunião entre amigos, aqui, todos são bem-vindos
+                para desfrutar da simplicidade e do sabor inigualável das comidas que nos remetem às origens e à tradição.
+                Convidamos você a se juntar a nós e explorar o verdadeiro prazer de uma refeição caseira, onde cada garfada
+                é recheada de afeto e tradição. No Sabor de Casa, a cozinha é mais do que uma arte, é uma experiência que 
+                alimenta não apenas o corpo, mas também a alma. Esperamos recebê-lo em breve para compartilhar momentos deliciosos
+                e memoráveis.."
             />
         </div>
     );

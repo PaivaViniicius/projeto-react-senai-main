faa565ba144af7634e161f2924c57a03e4b3d81d
VINÍCIUS DE SOUZA DE PAIVA <vinicius_paiva@estudante.sesisenai.org.br>
2023-12-06 21:27:12 -0300

    arrumando umas coisas

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,10 +10,14 @@ function Home() {
 
             <div class="img">
                 <img src={logoPrincipal} />
+
+                <InformationText
+                    pContent="Bem-vindos ao Maria Bonita, o restaurante que traz até você a autenticidade e o calor das comidas caseiras preparadas com carinho e tradição. Em nosso acolhedor ambiente, buscamos proporcionar uma experiência gastronômica única, onde cada prato é cuidadosamente elaborado para despertar memórias afetivas e sabores que remetem à comida feita em casa."
+                />
+
             </div>
-            <InformationText
-                pContent="Bem-vindos ao Maria Bonita, o restaurante que traz até você a autenticidade e o calor das comidas caseiras preparadas com carinho e tradição. Em nosso acolhedor ambiente, buscamos proporcionar uma experiência gastronômica única, onde cada prato é cuidadosamente elaborado para despertar memórias afetivas e sabores que remetem à comida feita em casa."
-            />
+
+
         </div>
 
 

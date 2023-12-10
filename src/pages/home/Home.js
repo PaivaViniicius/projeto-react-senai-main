76e59ea775581268e942a01e3e0a6a896facac81
Paiva Viniicius <lolcfthe01@gmail.com>
2023-12-10 19:44:20 -0300

    arrumando tela sobre

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className='inicial'>
 
-      <img className='background-image' src={imgHome} alt='Imagem de Fundo' />
+      <img className='background-image' src={imgHome}/>
 
 
 

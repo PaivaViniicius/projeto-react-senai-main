9bd2c767e65e262c5976c171176884d98b943198
mateuskons <mfkons@gmail.com>
2023-12-11 23:52:44 -0300

    imagem fundo produto

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,11 +1,12 @@
-
+import imgProduto from "../../img/imgProduto.png"
 function Product(){
 
     return(
         <div>
+            <img className='background-image-info' src={imgProduto} />
             <h1 className='title'>Produto</h1>
+
         </div>
     )
-
 }
 export default Product

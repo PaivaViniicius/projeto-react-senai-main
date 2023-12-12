8bda434ce6466d9e01690d03cad414371799eeaa
yurigrams <yurigrams@gmail.com>
2023-12-12 12:53:53 -0300

    Ajustada a tela de produtos

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -3,6 +3,7 @@ import InformationTextProduct from "../../components/informationTextProduct/info
 import "./Product.css"
 import ImgAcaraje from "../../img/imgAcaraje.jpg"
 import imgVatapa from "../../img/imgVatapa.jpg"
+import imgPaeja from "../../img/imgPaeja.jpg"
 function Product() {
 
     return (
@@ -30,6 +31,16 @@ function Product() {
                     pContent="O vatapá é um prato tradicional nordestino do Brasil, especialmente associado à Bahia. É um creme feito com pão, camarões secos, amendoim, 
                 leite de coco, azeite de dendê e temperos. Sua textura cremosa e sabor marcante o tornam um componente essencial em receitas como o acarajé. 
                 Além de ser apreciado por suas características gastronômicas, o vatapá possui relevância cultural, sendo parte integrante de festividades na região nordeste."
+                />
+            </div>
+            <div className="text-Inicial-Produto3">
+                <img className="imgAcaraje" src={imgPaeja} />
+                <InformationTextProduct
+                    h2Content="Paeja"
+                    pContent="A paella é um prato tradicional espanhol, originário da Comunidade Valenciana. Preparada em uma paellera, uma frigideira larga, 
+                    a paella combina arroz com ingredientes como frutos do mar, carne de frango, coelho e vegetais. Destaca-se pela mistura única de sabores e texturas. 
+                    É uma refeição social e festiva, frequentemente compartilhada entre amigos e familiares. Existem várias variações, como a paella Valenciana, 
+                    de frutos do mar e mista, cada uma refletindo a riqueza da tradição gastronômica espanhola."
                 />
             </div>
         </div>

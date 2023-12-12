9cf79744dd7f26f86cee5f0acc4292b2834a53a9
yurigrams <yurigrams@gmail.com>
2023-12-12 10:26:11 -0300

    Ajustada a tela de produtos

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -7,13 +7,15 @@ function Product() {
 
     return (
         <div>
-            <img className='background-img' src={imgProduto} />
+            <div className='background-img-container'>
+                <img className='background-img' src={imgProduto}/>
+            </div>
             <h1 className='title'>Comidas Tipicas</h1>
             <div className="text-Inicial-Produto">
-            <img className="imgAcaraje" src={ImgAcaraje}/>
+                <img className="imgAcaraje" src={ImgAcaraje} />
                 <InformationTextProduct
-                h2Content="Acarajé"
-                pContent="O acarajé é um prato tradicional da culinária afro-brasileira, originário da Bahia. 
+                    h2Content="Acarajé"
+                    pContent="O acarajé é um prato tradicional da culinária afro-brasileira, originário da Bahia. 
                 Consiste em uma massa frita feita de feijão-fradinho, cebola e sal, recheada com vatapá 
                 (creme de camarões secos, amendoim, leite de coco e temperos). Frito em azeite de dendê, 
                 o acarajé é conhecido por seus sabores intensos e é frequentemente associado a celebrações 
@@ -22,10 +24,10 @@ function Product() {
                 />
             </div>
             <div className="text-Inicial-Produto2">
-            <img className="imgAcaraje" src={imgVatapa}/>
+                <img className="imgAcaraje" src={imgVatapa} />
                 <InformationTextProduct
-                h2Content="Vatapá"
-                pContent="O vatapá é um prato tradicional nordestino do Brasil, especialmente associado à Bahia. É um creme feito com pão, camarões secos, amendoim, 
+                    h2Content="Vatapá"
+                    pContent="O vatapá é um prato tradicional nordestino do Brasil, especialmente associado à Bahia. É um creme feito com pão, camarões secos, amendoim, 
                 leite de coco, azeite de dendê e temperos. Sua textura cremosa e sabor marcante o tornam um componente essencial em receitas como o acarajé. 
                 Além de ser apreciado por suas características gastronômicas, o vatapá possui relevância cultural, sendo parte integrante de festividades na região nordeste."
                 />
